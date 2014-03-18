d39d818e2a1cd784e684be25c7f3a33634e569c5
vdv <dmitriy.vlasov@cetntre-it.com>
2014-03-18 11:45:41 +0600

    add js list manipulation binding

---
+++ PerpetuumSoft.Knockout/Scripts/listmanipulation.binding.js
@@ -0,0 +1,33 @@
+﻿function refresh(list) {
+	var data = list().slice(0);
+	list([]);
+	list(data);
+};
+
+ko.bindingHandlers.addItem = {
+	init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+		$(element).on('click', function () {
+			var params = valueAccessor();
+
+			var list = ko.unwrap(params.list);
+			var newItem = ko.mapping.fromJS(params.templateInstance);
+
+			list.push(newItem);
+			refresh(params.list);
+		});
+	}
+};
+
+ko.bindingHandlers.removeItem = {
+	init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+		$(element).on('click', function () {
+			var params = valueAccessor();
+
+			var list = ko.unwrap(params.list);
+			var index = params.index();
+
+			list.splice(index, 1);
+			refresh(params.list);
+		});
+	}
+};
